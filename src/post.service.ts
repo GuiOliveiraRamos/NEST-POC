98b23dc5555901bcb77194377c456b35b59d85fb
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2024-02-08 16:34:18 -0300

    feat: prisma implemented on nest project

---
+++ src/post.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "./prisma.service";
+import { Prisma } from "@prisma/client";
+
+@Injectable()
+
+export class PostService {
+    constructor(private prisma: PrismaService) { }
+
+    async post(
+        postWhereUniqueInput: Prisma.PostWhereUniqueInput,
+    ) {
+        return this.prisma.post.findUnique({
+            where: postWhereUniqueInput,
+        });
+    }
+
+    async posts(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.PostWhereUniqueInput;
+        where?: Prisma.PostWhereInput;
+        orderBy?: Prisma.PostOrderByWithRelationInput;
+    }) {
+        const { skip, take, cursor, where, orderBy } = params;
+        return this.prisma.post.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
+
+    async createPost(data: Prisma.PostCreateInput) {
+        return this.prisma.post.create({
+            data,
+        });
+
+    }
+
+    async updatePost(params: {
+        where: Prisma.PostWhereUniqueInput;
+        data: Prisma.PostUpdateInput;
+    }) {
+        const { where, data } = params;
+        return this.prisma.post.update({
+            data,
+            where,
+        });
+    }
+
+    async deletePost(where: Prisma.PostWhereUniqueInput) {
+        return this.prisma.post.delete({
+            where,
+        });
+    }
+
+}
